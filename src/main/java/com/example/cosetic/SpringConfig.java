89f93f9ccbaa27c72be0b1e25559ef7f497bb888
ayz1070 <ayz1070@gmail.com>
2025-01-07 17:57:19 +0900

    [dependency] 사용자 관련 의존성 주입
    - 의존성 주입

--- src/main/java/com/example/cosetic/SpringConfig.java
+++ src/main/java/com/example/cosetic/SpringConfig.java
@@ -1,23 +1,29 @@
 package com.example.cosetic;
 
-import com.example.cosetic.user.repository.MemoryUserRepository;
-import com.example.cosetic.user.repository.UserRepository;
-import com.example.cosetic.user.service.UserService;
+import com.example.cosetic.member.repository.MemberRepository;
+import com.example.cosetic.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 ///  의존성 주입 직접 하기
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public UserService userService() {
-        return new UserService(userRepository());
+    @Autowired
+    @Qualifier("springDataJpaMemberRepository") //
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(@Qualifier("springDataJpaMemberRepository") MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public UserRepository userRepository() {
-        return new MemoryUserRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 }
