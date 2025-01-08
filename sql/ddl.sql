651003de40c154081d31c6fba25becdbcb612df0
ayz1070 <ayz1070@gmail.com>
2025-01-08 17:48:11 +0900

    [sql] 배송 관련 sql문 작성
    - 배송 관련 sql문 작성

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -5,3 +5,13 @@ create table member
     name varchar(255),
     primary key (id)
 );
+
+
+
+CREATE TABLE shipping
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    order_id        BIGINT       NOT NULL,
+    tracking_number VARCHAR(255) NOT NULL,
+    carrier         VARCHAR(255) NOT NULL
+);
